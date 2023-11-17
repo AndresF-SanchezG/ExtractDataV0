1550f4474fdd79b8ca97401bd2b0e3481f53db98
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2023-11-17 13:05:22 -0500

    V5

--- dataframe.py
+++ dataframe.py
@@ -9,16 +9,28 @@ dataFrameHoteles['Desde'] = pd.to_datetime(dataFrameHoteles['Desde'])
 dataFrameHoteles['Hasta*'] = pd.to_datetime(dataFrameHoteles['Hasta*'])
 
 # Definir la fechaDesde
-fecha_desde = datetime.strptime('2024-01-03', '%Y-%m-%d')
-fecha_hasta = datetime.strptime('2024-01-15', '%Y-%m-%d')
+fecha_desde = datetime.strptime('2023-12-19', '%Y-%m-%d')
+fecha_hasta = datetime.strptime('2023-12-30', '%Y-%m-%d')
 
 # Filtrar por Hotel y fechas
-filtro = (dataFrameHoteles['Hotel'] == 'Barú') & (dataFrameHoteles['Desde'] <= fecha_hasta) & (dataFrameHoteles['Hasta*'] >= fecha_desde)
-df_filtrado = dataFrameHoteles[filtro]
+filtro = (dataFrameHoteles['Hotel'] == 'Maryland') & (dataFrameHoteles['Desde'] <= fecha_hasta) & (dataFrameHoteles['Hasta*'] >= fecha_desde)
+df_filtrado = dataFrameHoteles[filtro].copy()  # Agregar .copy() para evitar el SettingWithCopyWarning
 
-# Imprimir solo el contenido del DataFrame filtrado
-print('Contenido del DataFrame filtrado:')
-print(df_filtrado)
+# Calcular la nueva columna cant/dias utilizando .apply
+df_filtrado['cant_dias'] = df_filtrado.apply(lambda row: (min(row['Hasta*'], fecha_hasta) - max(row['Desde'], fecha_desde)).days + 1, axis=1)
+
+# Identificar la última fila de cada grupo de habitaciones y restar 1 solo a esa fila
+last_rows = df_filtrado.duplicated(subset='Habitación', keep='last')
+print(last_rows)
+df_filtrado.loc[df_filtrado.index.isin(last_rows[last_rows == False].index), 'cant_dias'] -= 1
+
+# Imprimir el contenido del DataFrame filtrado con la nueva columna
+print('Contenido del DataFrame filtrado con la nueva columna:')
+print(df_filtrado[['Hotel', 'Habitación', 'Desde', 'Hasta*', 'Descuento', 'Sencilla', 'Doble/Adicional', 'Niño', 'cant_dias']])
+
+
+
+
 
 
 
